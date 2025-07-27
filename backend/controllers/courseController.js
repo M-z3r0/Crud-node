c655f4d91653655c8a79e837533dfa2a88ba33ca
Murilo <munery0205@gmail.com>
2025-07-26 21:56:26 -0300

    Inserindo perfeitamente no banco

--- backend/controllers/courseController.js
+++ backend/controllers/courseController.js
@@ -1,11 +1,35 @@
 const db = require('../models/db');
-exports.insertCourse = (req,res)=>{
-    const {nomeICP,duracaoICP,descICP,imagemICP} = req.body;
-    db.query("INSERT INTO cursos(curso_name,curso_duracao,curso_desc,curso_foto) VALUES($1,$2,$3,$4) RETURNING id",[nomeICP,duracaoICP,descICP,imagemICP],(err,result)=>{
-        if(err){
-            return res.status(500).json(err);
+exports.insertCourse = (req, res) => {
+    const { nomeICP, duracaoICP, descICP, imagemICP } = req.body;
+
+    console.log("Dados recebidos:", {
+        nomeICP,
+        duracaoICP,
+        descICP,
+        imagemICP: imagemICP?.slice(0, 50) + '...'
+    });
+
+    const sql = `
+        INSERT INTO cursos(curso_name, curso_duracao, curso_desc, curso_foto)
+        VALUES ($1, $2, $3, $4)
+        RETURNING curso_id
+    `;
+
+    db.query(sql, [nomeICP, duracaoICP, descICP, imagemICP], (err, result) => {
+        if (err) {
+            console.error("Erro ao inserir:", err); // mostra erro no terminal
+            return res.status(500).json({ erro: err.message });
         }
-        res.sendStatus(201).json({id:result.rows[0].id,nomeICP,duracaoICP,descICP,imagemICP});
+
+        console.log("Inserido com sucesso:", result.rows[0]);
+
+        res.status(201).json({
+            id: result.rows[0].id,
+            nomeICP,
+            duracaoICP,
+            descICP,
+            imagemICP
+        });
     });
 };
 exports.selectCourse = (req,res)=>{
